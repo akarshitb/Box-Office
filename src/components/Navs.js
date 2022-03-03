7ccf8582a7f472241c804789fd53419e8364dbac
Akarshit Baru <akarshitbaru1@gmail.com>
2022-03-03 20:24:03 +0530

    Added navbar and pages

---
+++ src/components/Navs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Links = [
+  {
+    to: "./",
+    text: "Home",
+  },
+  { to: "./starred", text: "Starred" },
+];
+
+const navs = () => {
+  return (
+    <div>
+      <ul>
+        {Links.map((items) => (
+          <li key={items.to}>
+            <Link to={items.to}>{items.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default navs;
